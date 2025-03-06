9b73b4277a9e4db80e28a06d0637ddc33a70738f
rindo57 <77073061+rindo57@users.noreply.github.com>
2025-03-07 02:54:08 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,11 +5,11 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    MONGO_URI = os.getenv('MONGO_URI')
+    SECRET_KEY = "asdf324fdhgfbbdfg"
+    MONGO_URI = 'mongodb+srv://diablo:OH4WLGrCZOlG6FH6@cluster0.qokt3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
     ALLOWED_EXTENSIONS = {'pptx', 'docx', 'xlsx'}
     ROOT_DIR = os.path.abspath(os.curdir)
-    BASE_URL = os.getenv('BASE_URL') or 'http://127.0.0.1:5000/'
+    BASE_URL = os.getenv('BASE_URL') or 'http://0.0.0.0:80/'
     SMTP2GO_API_KEY = os.getenv('SMTP2GO_API_KEY')
     SMTP2GO_SENDER = os.getenv('SMTP2GO_SENDER')
