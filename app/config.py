09ceb87e35e00a3b35148ee8b36b73dcbbb8ee15
rindo57 <77073061+rindo57@users.noreply.github.com>
2025-03-07 02:57:50 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -10,6 +10,6 @@ class Config:
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
     ALLOWED_EXTENSIONS = {'pptx', 'docx', 'xlsx'}
     ROOT_DIR = os.path.abspath(os.curdir)
-    BASE_URL = os.getenv('BASE_URL') or 'http://0.0.0.0:80/'
+    BASE_URL = 'http://0.0.0.0:80/'
     SMTP2GO_API_KEY = os.getenv('SMTP2GO_API_KEY')
     SMTP2GO_SENDER = os.getenv('SMTP2GO_SENDER')
